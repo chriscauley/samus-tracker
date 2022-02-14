ac9a5b02a70312573c0a80e0eed545d4ad11605b
chriscauley <chris@lablackey.com>
2022-02-14 15:42:36 -0500

    saving/loading now works via tap/hold mechanism

--- scripts/hax.py
+++ scripts/hax.py
@@ -1,7 +1,6 @@
 from inputs import get_gamepad, UnknownEventCode
 import time
 # import keyboard
-from pynput.keyboard import Key, Controller
 import pyautogui
 
 
@@ -12,22 +11,29 @@ KEY_TO_CODE = {
     'A': 'BTN_SOUTH',
     'B': 'BTN_EAST',
 }
-controller = Controller()
-def press(key):
-    print('pressing', key)
-    pyautogui.keyDown(key)
-    time.sleep(0.1)
-    print('releasing', key)
-    pyautogui.keyUp(key)
-
 CODE_TO_KEY = {}
 
 for a,b in KEY_TO_CODE.items():
     CODE_TO_KEY[b] = a
 
+def press(key, text='pressed'):
+    print(f'{key} {text}...', end="", flush=True)
+    pyautogui.keyDown(key)
+    time.sleep(0.1)
+    print('done!', key)
+    pyautogui.keyUp(key)
+
+
+def split(key):
+    # TODO get LiveSplitOne working locally so we can use https://pypi.org/project/livesplit/
+    pyautogui.hotkey('alt', 'tab')
+    pyautogui.press(key)
+    pyautogui.hotkey('alt', 'tab')
+
 
 def main():
     """Just print out some event infomation when the gamepad is used."""
+    last_tap = 0
     r_down = False
     r_lock = False
     need_clear = False
@@ -39,27 +45,38 @@ def main():
         for event in events:
             key = CODE_TO_KEY.get(event.code)
             if not key:
+                # TODO add all to key map
+                print('unknown code', event.code)
                 continue
             if key == 'R':
-                r_down = bool(event.state)
                 r_lock = False
                 if event.state:
-                    r_down = True
+                    r_down = time.time()
                 else:
+                    duration = time.time() - r_down
+                    if duration < 0.5:
+                        if time.time() - last_tap < 0.5:
+                            press('f2', 'saving')
+                        else:
+                            print('tappy...')
+                            last_tap = time.time()
+                    elif duration < 2:
+                        press('f4', 'loading')
                     r_down = False
+
                 continue
-            if r_lock or not r_down:
+
+            # Every key after this is triggered when pressed
+            if not event.state:
                 continue
-            if event.state:
+
+            if r_down and not r_lock:
+                # Player must be holding r and press one of these
                 r_lock = True
                 if key == 'A':
                     press(' ')
-                if key == 'B':
+                elif key == 'B':
                     press('m')
-                if key == 'X':
-                    press('f2')
-                if key == 'Y':
-                    press('f4')
 
 
 if __name__ == "__main__":
