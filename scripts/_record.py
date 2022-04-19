17d89584de1d9c7be8b4d8540f4f5b91b0b2f79b
chriscauley <chris@lablackey.com>
2022-04-19 06:46:37 -0400

    speedbooster and record scripts

---
+++ scripts/_record.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+from mss import mss
+from pathlib import Path
+from PIL import Image
+import time
+import urcv
+import sys
+
+# define dimensions of screen w.r.t to given monitor to be captured
+target = sys.argv[1]
+path = Path(sys.argv[1])
+
+x, y, w, h = [0, 64, 1024, 896]
+
+if sys.argv[1] == '--bounds':
+    x, y, w, h  = [0,0,1400,1080]
+else:
+    print('starting in 3...2...1')
+    time.sleep(3)
+    if not path.exists():
+        path.mkdir()
+
+options = {
+    'left': x,
+    'width': w,
+    'top': y,
+    'height': h,
+}
+
+last = 0
+i = 0
+i_max = 160
+with mss() as sct:
+    last = None
+    while True:
+        screenShot = sct.grab(options)
+        img = Image.frombytes(
+            'RGB',
+            (screenShot.width, screenShot.height),
+            screenShot.rgb,
+        )
+        frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        if sys.argv[1] == '--bounds':
+            print(urcv.get_scaled_roi(frame, 1, pixel_fix=True))
+            exit()
+        if last != np.sum(frame):
+            print(f'{path}/{i:04d}.png')
+            cv2.imwrite(f'{path}/{i:04d}.png', frame)
+            i += 1
+            last = np.sum(frame)
+            if i > i_max:
+                break
+
+
+        # img = Image.frombytes(
+        #     'RGB',
+        #     (screenShot.width, screenShot.height),
+        #     screenShot.rgb,
+        # )
+        # frame = np.array(img)
+        # mini_h = int(GAME_LEFT * GAME_HEIGHT / GAME_WIDTH)
+        # mini = cv2.resize(frame, (GAME_LEFT, mini_h))
+        # gray_mini = cv2.cvtColor(mini, cv2.COLOR_RGB2GRAY)
+        # frames.append(time.time())
+        # frames = [f for f in frames if f > time.time() - 5]
+        # if last is not None:
+        #     frameDelta = cv2.absdiff(last, gray_mini)
+        #     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        #     thresh = cv2.dilate(thresh, None, iterations=2)
+        #     diff = cv2.bitwise_and(mini, mini, mask=thresh)
+        #     urcv.text.write(diff, f'fps: {int(10*len(frames)/5)/10}')
+        #     cv2.imshow('diff', diff)
+
+        # cv2.imshow('test', mini)
+        # pressed = urcv.wait_key(max_time=1, delay=33)
+        # if pressed == 'q':
+        #     break
+        # last = gray_mini
--- scripts/_speedbooster.py
+++ scripts/_speedbooster.py
@@ -5,29 +5,12 @@ import numpy as np
 from pathlib import Path
 import sys
 import urcv
-from unrest.utils import time_it
+from unrest.utils import time_it, JsonCache
 
-root_dir = Path(f'.cache/speedbooster/{sys.argv[1]}')
-frames_dir = root_dir / 'frames'
+import matplotlib.pyplot as plt
 
-
-if not frames_dir.exists() or '--reset' in sys.argv:
-    def _sorter(s):
-        return s.replace('-1.png', '1.png')
-    screenshots = sorted([str(s) for s in root_dir.iterdir() if str(s).endswith('.png')], key=_sorter)
-    frames_dir.mkdir(exist_ok=True)
-    ss0 = cv2.imread(screenshots[0])
-    bounds = urcv.input.get_scaled_roi(ss0, 1, pixel_fix=True)
-    for i, ss in enumerate(screenshots):
-        fixed = urcv.transform.crop(cv2.imread(ss), bounds)
-        cv2.imwrite(str(frames_dir / f'{i}.png'), fixed)
-    print(f'wrote {len(screenshots)} frames')
-
-canvas = None
-og_canvas = None
-gray_canvas = None
-
-dw = 0
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
 
 def compactify(image):
     image = image[::4, ::4] # down pixel
@@ -63,47 +46,95 @@ def invert_match(back, front):
     return values.index(min_)
 
 
-for i_frame in range(len(list(frames_dir.iterdir()))):
-    frame = og = cv2.imread(f'{frames_dir}/{i_frame}.png')
-    frame = compactify(og)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    if i_frame == 0:
-        # first loop, start canvas
-        canvas = frame.copy()
-        og_canvas = frame.copy()
-        gray_canvas = gray
-        continue
-
-    w, h = frame.shape[:2]
-    template = gray[:, 0:64]
-    template[16:-32] = 0
-    target = gray_canvas[:,dw:dw+128]
-    target[16:-32] = 0
+def get_box(frame):
+    ys, xs = np.where(np.all(frame == (0,0,255), axis=-1))
+    return (min(*xs), min(*ys), max(*xs), max(*ys))
 
 
-    dx = invert_match(target, template)
-    if not dx:
-        continue
+def main(number, show=False):
+    frames_dir = Path(f'.cache/speedbooster/{number}')
 
-    # abandoned attempt at matching using opencv's template matching
-    # matches = urcv.template.match(target, template)
-    # if len(matches):
-    #     print(dx, matches[0])
+    xs = []
+    canvas = None
+    og_canvas = None
+    gray_canvas = None
+    dw = 0
+    last = 0
 
-    # draw a white line to prevent back shifting
-    canvas[:,dw] = 255
-    gray_canvas[:,dw] = 255
-    target[0:5,dx] = 255
+    for i_frame in range(len(list(frames_dir.iterdir()))):
+        og = cv2.imread(f'{frames_dir}/{i_frame:04d}.png')
+        last = np.sum(og)
+        frame = compactify(og)
+        urcv.replace_color(frame, (255, 65, 66), (0,0,255))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    dw += dx
-    canvas = urcv.draw.paste_expandable(canvas, frame, dw, 0)
-    gray_canvas = urcv.draw.paste_expandable(gray_canvas, gray, dw, 0)
-    og_canvas = urcv.draw.paste(np.zeros(canvas.shape, dtype=np.uint8), og_canvas, 0, 0)
+        if i_frame == 0:
+            # first loop, start canvas
+            canvas = frame.copy()
+            og_canvas = frame.copy()
+            gray_canvas = gray
+            continue
 
-    cv2.imshow('template', template)
-    cv2.imshow('target', target)
-    cv2.imshow('canvas', np.vstack([canvas, og_canvas[-32:]]))
-    cv2.imshow('frame', frame)
-    pressed = urcv.wait_key()
+        w, h = frame.shape[:2]
+        template = gray[:, 0:64]
+        template[16:-32] = 0
+        target = gray_canvas[:,dw:dw+128]
+        target[16:-32] = 0
 
+
+        dx = invert_match(target, template)
+        box_bounds = get_box(frame)
+        xs.append(box_bounds[0] + dw)
+
+        # draw a white line to prevent back shifting
+        canvas[:,dw] = 255
+        gray_canvas[:,dw] = 255
+        target[0:5,dx] = 255
+
+        dw += dx
+        canvas = urcv.draw.paste_expandable(canvas, frame, dw, 0)
+        gray_canvas = urcv.draw.paste_expandable(gray_canvas, gray, dw, 0)
+        og_canvas = urcv.draw.paste(np.zeros(canvas.shape, dtype=np.uint8), og_canvas, 0, 0)
+
+        if show:
+            cv2.imshow('template', template)
+            cv2.imshow('target', target)
+            cv2.imshow('canvas', urcv.transform.scale(canvas, 2))
+            cv2.imshow('frame', frame)
+            pressed = urcv.wait_key()
+
+            if pressed == 'q':
+                break
+
+    dxs = []
+    for i, x in enumerate(xs):
+        if i:
+            dxs.append(x-xs[i-1])
+
+    if show:
+        plt.plot(dxs)
+        plt.plot(moving_average(dxs, 4))
+        plt.ylabel('dxs')
+        plt.show()
+    return [int(i) for i in dxs]
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'all':
+        folders = Path('.cache/speedbooster').iterdir()
+        folders = [f for f in folders if str(f).split('/')[-1].isdigit()]
+        cached = JsonCache('.cache/speedbooster/data.json')
+        for i,f in enumerate(folders):
+            if not str(f) in cached:
+                print('processed', f)
+                cached[str(f)] = main(str(f).split('/')[-1])
+            dxs = cached[str(f)]
+            # plt.plot(dxs)
+            # i/20 here is because the lines are too close to differentiate
+            data = moving_average(dxs, 4)
+            # data = [dx+i/20 for dx in data]
+            plt.plot(data)
+        plt.ylabel('dxs')
+        plt.show()
+    else:
+        main(sys.argv[1], show = True)
